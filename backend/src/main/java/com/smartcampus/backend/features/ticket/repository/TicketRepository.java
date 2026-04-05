895c76d90c5507e6c7540632d523c01d0ec87be3
PraneeshaPerera <praneesha2003perera@gmail.com>
2026-04-05 10:21:19 +0530

    Revert "Feature/ticket"

--- backend/src/main/java/com/smartcampus/backend/features/ticket/repository/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartcampus.backend.features.ticket.repository;
-
-import com.smartcampus.backend.features.ticket.model.Ticket;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketRepository extends MongoRepository<Ticket, String> {
-}
